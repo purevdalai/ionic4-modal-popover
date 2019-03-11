aaa875db980836f945c7746ba79f8738ba3446f7
Purevdalai <purewdalai3@gmail.com>
2019-03-11 10:50:34 +0800

    Complete Modal, Pages, Popover

---
+++ src/app/pages/second/second.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-second',
+  templateUrl: './second.page.html',
+  styleUrls: ['./second.page.scss'],
+})
+export class SecondPage implements OnInit {
+
+  passedId = null;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.passedId = this.activatedRoute.snapshot.paramMap.get('myid');
+  }
+
+}
